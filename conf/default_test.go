1d500285024dab91b4ec2114e49d880713459a50
Alec Savvy <alecjsavvy@gmail.com>
2023-11-18 10:10:06 -0700

    start to prod and stage defaults

--- conf/default_test.go
+++ conf/default_test.go
@@ -6,7 +6,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDevDefaults(t *testing.T) {
+func TestDefaults(t *testing.T) {
 	devDefaults := GetDevDefaults()
 	require.EqualValues(t, "dev", devDefaults.Network.Name)
+
+	stageDefaults := GetStageDefaults()
+	require.EqualValues(t, "stage", stageDefaults.Network.Name)
+
+	prodDefaults := GetProdDefaults()
+	require.EqualValues(t, "prod", prodDefaults.Network.Name)
 }
