567d61d347b4c7c9f825b6e364fdb7fbecd611d3
endline <endline@users.noreply.github.com>
2023-11-25 11:57:55 -0700

    Port node-ui code from protocol repo

---
+++ ui/src/hooks/useMinChainVersions.ts
@@ -0,0 +1,44 @@
+import { useEthContract } from './useEthContracts'
+import serviceTypeManagerJSON from '../eth-contracts/ABIs/ServiceTypeManager.json'
+import { utf8ToBytes32, bytes32ToUtf8 } from '../utils/utils'
+
+export const serviceType = Object.freeze({
+  DISCOVERY_PROVIDER: 'discovery-node',
+  CREATOR_NODE: 'content-node'
+})
+
+const useMinChainVersions = () => {
+  const expectedVersions: Record<string, string | null | undefined> = {}
+
+  const {
+    data: discoveryResult,
+    isPending: isDiscoveryResultPending,
+    error: discoveryResultError
+  } = useEthContract(serviceTypeManagerJSON, 'getCurrentVersion', [
+    utf8ToBytes32(serviceType.DISCOVERY_PROVIDER)
+  ])
+  if (!isDiscoveryResultPending && !discoveryResultError && discoveryResult) {
+    expectedVersions[serviceType.DISCOVERY_PROVIDER] = bytes32ToUtf8(discoveryResult)
+  }
+
+  const {
+    data: contentResult,
+    isPending: isContentResultPending,
+    error: contentResultError
+  } = useEthContract(serviceTypeManagerJSON, 'getCurrentVersion', [
+    utf8ToBytes32(serviceType.CREATOR_NODE)
+  ])
+  if (!isContentResultPending && !contentResultError && contentResult) {
+    expectedVersions[serviceType.CREATOR_NODE] = bytes32ToUtf8(contentResult)
+  }
+
+  const isPending = isDiscoveryResultPending || isContentResultPending
+  const error = discoveryResultError || contentResultError
+  return {
+    data: expectedVersions,
+    isPending,
+    error
+  }
+}
+
+export default useMinChainVersions
