9b2d38ae6c8913cbddb0be95076f44a87b86aa6f
Raymond Jacobson <ray@audius.co>
2023-12-18 15:32:01 -0800

    Add necessary polyfills

--- web/ui/vite.config.ts
+++ web/ui/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
   plugins: [
@@ -14,6 +15,30 @@ export default defineConfig({
       protocolImports: true,
     }),
   ],
+  resolve: {
+    alias: {
+      os: "os-browserify",
+      path: "path-browserify",
+      url: "url",
+      zlib: "browserify-zlib",
+      crypto: "crypto-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      stream: "stream-browserify",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
   // Set to /d/ in Dockerfile. Leave unset ('/') if deploying standalone in the future (e.g., to Cloudflare Pages).
   base: process.env.UPTIME_BASE_URL || "/",
   build: {
